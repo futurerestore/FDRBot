6c84067c81c431bc81e33eb00bfe995916c7505d
m1stadev <adamhamdi31@gmail.com>
2022-02-24 22:51:45 -0600

    [events] Overhaul - Remove now-unnecessary code - Implement proper error handler for commands - Events -> EventsCog

--- cogs/events.py
+++ cogs/events.py
@@ -1,125 +1,71 @@
-from aioify import aioify
 from discord.ext import commands
-import aiosqlite
+
 import discord
-import os
 
 
-class Events(commands.Cog):
+class EventsCog(discord.Cog, name='Events'):
     def __init__(self, bot):
         self.bot = bot
-        self.os = aioify(os, name='os')
-        self.utils = self.bot.get_cog('Utils')
 
-
-    @commands.Cog.listener()
-    async def on_guild_join(self, guild: discord.Guild) -> None:
-        await self.bot.wait_until_ready()
-
-        async with aiosqlite.connect('Data/fdrbot.db') as db:
-            async with db.execute('SELECT prefix from prefix WHERE guild = ?', (guild.id,)) as cursor:
-                if await cursor.fetchone() is not None:
-                    await db.execute('DELETE from prefix where guild = ?', (guild.id,))
-                    await db.commit()
-
-            await db.execute('INSERT INTO prefix(guild, prefix) VALUES(?,?)', (guild.id, '!'))
-            await db.commit()
-
-
-        embed = await self.utils.info_embed('!', self.bot.user)
-        for channel in guild.text_channels:
-            try:
-                await channel.send(embed=embed)
-                break
-            except:
-                pass
-
-    @commands.Cog.listener()
-    async def on_guild_remove(self, guild: discord.Guild) -> None:
-        await self.bot.wait_until_ready()
-
-        async with aiosqlite.connect('Data/fdrbot.db') as db:
-            await db.execute('DELETE from prefix where guild = ?', (guild.id,))
-            await db.commit()
-
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message) -> None:
-        await self.bot.wait_until_ready()
-
-        if message.channel.type == discord.ChannelType.private:
-            return
-
-        if message.content.replace(' ', '').replace('!', '') == self.bot.user.mention:
-            prefix = await self.utils.get_prefix(message.guild.id)
-
-            embed = discord.Embed(title=self.bot.user.name, description=f'My prefix is `{prefix}`. To see all of my commands, run `{prefix}help`.')
-            embed.set_footer(text=message.author.name, icon_url=message.author.avatar_url_as(static_format='png'))
-            try:
-                await message.reply(embed=embed)
-            except:
-                pass
-
-    @commands.Cog.listener()
+    @discord.Cog.listener()
     async def on_ready(self) -> None:
-        await self.os.makedirs('Data', exist_ok=True)
-
-        async with aiosqlite.connect('Data/fdrbot.db') as db:
-            await db.execute('''
-                CREATE TABLE IF NOT EXISTS tags(
-                name STRING,
-                text STRING,
-                creator INTEGER
-                )
-                ''')
-            await db.commit()
-
-            await db.execute('''
-                CREATE TABLE IF NOT EXISTS prefix(
-                guild INTEGER,
-                prefix TEXT
-                )
-                ''')
-            await db.commit()
-
-        await self.bot.change_presence(activity=discord.Game(name='Ping me for help!'))
         print('FDRBot is now online.')
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx: commands.Context, error) -> None:
+    @discord.Cog.listener()
+    async def on_application_command_error(
+        self, ctx: discord.ApplicationContext, exc: commands.errors.CommandError
+    ) -> None:
         await self.bot.wait_until_ready()
 
-        if ctx.message.channel.type == discord.ChannelType.private:
-            return
+        if isinstance(exc, discord.ApplicationCommandInvokeError):
+            exc = exc.__cause__
 
         embed = discord.Embed(title='Error')
+        if isinstance(exc, commands.errors.NoPrivateMessage):
+            embed.description = 'This command can only be used in a server.'
 
-        prefix = await self.utils.get_prefix(ctx.guild.id)
-        if isinstance(error, commands.CommandNotFound):
-            if ctx.prefix.replace('!', '').replace(' ', '') == self.bot.user.mention:
-                return
+        elif isinstance(
+            exc,
+            (commands.errors.MissingPermissions, commands.errors.BotMissingPermissions),
+        ):
+            missing_perms = [
+                perm.replace('_', ' ').replace('guild', 'server').title()
+                for perm in exc.missing_permissions
+            ]
 
-            embed.description = f"That command doesn't exist! Use `{prefix}help` to see all the commands I can run."
-            await ctx.reply(embed=embed)
+            if len(missing_perms) > 2:
+                fmt = '{}, and {}'.format(
+                    ", ".join(missing_perms[:-1]), missing_perms[-1]
+                )
+            else:
+                fmt = ' and '.join(missing_perms)
 
-        elif isinstance(error, commands.MaxConcurrencyReached):
-            embed.description = f"`{prefix + ctx.command.qualified_name}` cannot be ran more than once at the same time!"
-            await ctx.reply(embed=embed)
+            if isinstance(exc, commands.errors.MissingPermissions):
+                embed.description = f'You are missing the following permissions required to run this command: {fmt}.'
+            elif isinstance(exc, commands.errors.BotMissingPermissions):
+                embed.description = f'I am missing the following permissions required to run this command: {fmt}.'
 
-        elif isinstance(error, commands.errors.CommandInvokeError):
-            if not isinstance(error.original, discord.errors.Forbidden):
-                raise error
+        elif isinstance(exc, commands.errors.BadArgument):
+            embed.description = str(exc)
 
-        elif isinstance(error, commands.ChannelNotFound):
-            embed = discord.Embed(title='Error', description='That channel does not exist.')
-            await ctx.reply(embed=embed)
-
-        elif (isinstance(error, commands.errors.NotOwner)) or \
-        (isinstance(error, commands.MissingPermissions)):
-            return
+        elif isinstance(exc, commands.errors.UserNotFound):
+            embed.description = 'I could not find that user.'
 
         else:
-            raise error
+            owner = await self.bot.fetch_user(self.bot.owner_id)
+            embed.description = (
+                f'An unknown error occurred, please report this to {owner.mention}!'
+            )
+            embed.add_field(
+                name='Error Info',
+                value=f'Command: `/{ctx.command.qualified_name}`\nError message: `{str(exc)}`',
+            )
+
+        if not ctx.interaction.response.is_done():
+            await ctx.respond(embed=embed, ephemeral=True)
+        else:
+            await ctx.edit(embed=embed)
 
 
 def setup(bot):
-    bot.add_cog(Events(bot))
+    bot.add_cog(EventsCog(bot))
